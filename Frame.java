5eb24ac44fad3a1b94d5b00957d7af71ea8a0f42
ChrisMin3 <88865916+ChrisMin3@users.noreply.github.com>
2024-01-13 01:34:27 -0500

    Add files via upload

--- Frame.java
+++ Frame.java
@@ -11,15 +11,16 @@ public class Frame extends JFrame {
         
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
-        JLabel title = placeImage("title.png", 0.163, 0.2);
+        JLabel title = placeImage("title.png", 771, 400);
         this.add(title);
 
-        JLabel startIcon = placeImage("startIcon.png", 0.32, 0.04);
+        JLabel startIcon = placeImage("startIcon.png", 1050, 220);
         this.add(startIcon);
 
-        JLabel startButton = placeImage("startButton.png", 0.43, 0.5);
+        JLabel startButton = placeImage("startButton.png", 1167, 720);
         this.add(startButton);
 
+        // Default frame setup
         ImageIcon icon = new ImageIcon("icon.png");
         this.setIconImage(icon.getImage()); // Change icon of frame
         this.getContentPane().setBackground(new Color(28,145,163)); // Change the color of background
@@ -33,7 +34,7 @@ public class Frame extends JFrame {
         
     }
 
-    public JLabel placeImage(String file, double widthMultiplier, double heightMultiplier) {
+    public JLabel placeImage(String file, int x, int y) {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         double screenHeight = screenSize.getHeight();
         double screenWidth = screenSize.getWidth();
@@ -41,16 +42,15 @@ public class Frame extends JFrame {
         // JLabel label = new JLabel();
         ImageIcon image = new ImageIcon(file);
         
-        int imageWidth = image.getIconWidth();
-        int imageHeight = image.getIconHeight();
+        int resizedWidth = (int)(image.getIconWidth()*(screenWidth/2560));
+        int resizedHeight = (int)(image.getIconHeight()*(screenHeight/1440));
 
         try {
             BufferedImage tempImage = ImageIO.read(getClass().getResource(file));
-            Image resizedImage = tempImage.getScaledInstance((int)((imageWidth/screenWidth)*900), (int)((imageHeight/screenHeight)*600), Image.SCALE_SMOOTH);
+            Image resizedImage = tempImage.getScaledInstance(resizedWidth, resizedHeight, Image.SCALE_SMOOTH);
             JLabel label = new JLabel(new ImageIcon(resizedImage));
 
-            // label.setIcon(image);
-            label.setBounds((int)(screenWidth*widthMultiplier), (int)(screenHeight*heightMultiplier), imageWidth, imageHeight);
+            label.setBounds((int)(x*(screenWidth/2560)), (int)(y*(screenHeight/1440)), resizedWidth, resizedHeight);
 
             return label;
         } catch (IOException e) {
